8d14e036d6ea69a2f4aeda60233ade04e022ff2b
fiaphacka <132756088+fiaphacka@users.noreply.github.com>
2023-05-06 15:44:49 -0300

    Update db.py

--- db.py
+++ db.py
@@ -2,23 +2,23 @@ import os
 import pymysql
 from flask import jsonify
 
-db_user = os.environ.get('CLOUD_SQL_USERNAME')
-db_password = os.environ.get('CLOUD_SQL_PASSWORD')
-db_name = os.environ.get('CLOUD_SQL_DATABASE_NAME')
-db_local_host = os.environ.get('DB_LOCAL_HOST')
-db_connection_name = os.environ.get('CLOUD_SQL_CONNECTION_NAME')
+db_user = 'playuser'
+db_password = '123456'
+db_name = 'playlist'
+db_local_host = '34.168.105.218'
+db_connection_name = 'db'
 
 
 def open_connection():
     try:
-        if db_connection_name:
-            unix_socket = '/cloudsql/{}'.format(db_connection_name)
-            conn = pymysql.connect(user=db_user, password=db_password,
-                                   unix_socket=unix_socket, db=db_name,
-                                   cursorclass=pymysql.cursors.DictCursor
-                                   )
-            print('tchau')
-        else:
+        #if db_connection_name:
+        #    unix_socket = '/cloudsql/{}'.format(db_connection_name)
+        #    conn = pymysql.connect(user=db_user, password=db_password,
+        #                           unix_socket=unix_socket, db=db_name,
+        #                           cursorclass=pymysql.cursors.DictCursor
+        #                           )
+        #    print('tchau')
+        #else:
             print('oi')
             conn = pymysql.connect(user=db_user, password=db_password,
                                    host=db_local_host, db=db_name, cursorclass=pymysql.cursors.DictCursor)
